f89896997fea3fb1660653c74a79e52d781cdb2a
samragyee-d <samragyeedhakal@gmail.com>
2022-11-05 17:14:39 -0400

    Adding comments, print is not necessary

--- Carbon Emissions 2.py
+++ Carbon Emissions 2.py
@@ -1,23 +1,36 @@
-from flask import Flask
+#Importing pandas
 import pandas as pd
 
-
+#Establishing a file route
 df_carbondata = pd.read_csv(r"C:\Users\Hem Dhakal BCCC\Downloads\Countries and Territories (1).csv")
 
-
+#Establising variable that will store the emissions data for each income level. 
+#Inside the df_carbondata.loc call carbon data, and everything in the brackets get the data based on the income level. 
 df_LowIncome = df_carbondata.loc[df_carbondata.IncomeLevel=='Low Income', "Carbon"]
 df_LowerMiddleIncome = df_carbondata.loc[df_carbondata.IncomeLevel=='Lower Middle Income', "Carbon"]
 df_UpperMiddleIncome = df_carbondata.loc[df_carbondata.IncomeLevel=='Upper Middle Income', "Carbon"]
 df_HighIncome = df_carbondata.loc[df_carbondata.IncomeLevel=='High Income', "Carbon"]
 
+#Establising global variables
 global sumCO
 global lenCO
 global avgCO
 global rounded_avgCO
 
+#Function with the input as a parameter
+#if statement says that if the parameter equals a certain income level (str) then 
+# the function should equate the input to the variables established above that holds carbon data. 
+# That value is passed to the "sumCO" varibale which equates to a pandas function "sum" that holds 
+# the variable with the carbon data. It adds up all of the data in the variable
+# that data then goes through the "lenCO" variable which equates to a pandas function "len" that also holds carbon data.
+# "len" determines the length of the data
+# "avgCO" is a variable that equates to the "sumCO" being divided by the "lenCO" which gives us the average. 
+# "avgCO" is then taken to another variable called "rounded_avgCO" which equates to the pandas round function that holds 
+# "avgCO". 
+#The result is printed
 def CalcAvg (InputIncome):
     if InputIncome == "Low Income":
-        InputIncome = df_LowIncome.round(1)
+        InputIncome = df_LowIncome
         sumCO = sum(df_LowIncome)
         lenCO = len(df_LowIncome)
         avgCO = sumCO/lenCO
@@ -43,5 +56,5 @@ def CalcAvg (InputIncome):
         lenCO = len(df_HighIncome)
         avgCO = sumCO/lenCO
         rounded_avgCO = round(avgCO)
-        print(rounded_avgCO)
 result = CalcAvg("Low Income") 
+
